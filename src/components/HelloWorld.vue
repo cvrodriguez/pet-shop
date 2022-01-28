45bc6710e9eea085a9800cdcda965ff82e9462dc
Cindy Viviana Rodriguez R <cinvir15@gmail.com>
2022-01-28 21:40:46 +0100

    listed and filter

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,105 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="form-group">
+      <label> Nombre</label>
+      <input type="text" class="form-control" v-model="filterField"/>
+    </div>
+    <table class="table caption-top">
+      <caption>
+        List of users
+      </caption>
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody >
+        <tr v-for="animal in filteredList" :key="animal.name"  >
+          <th scope="row" ></th>
+          <td>{{ animal.name }}</td>
+          <td>{{ animal.lastname }}</td>
+          <td>{{ animal.type }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <button class="btn btn-primary">Bootstrap collapse</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
+    msg: String,
+  },
+  data() {
+    return {
+      animals: [
+        {
+          name: "Pocha ",
+          lastname: "Rodriguez ",
+          type: "Cat",
+          age: 0,
+          rh: "0+",
+        },
+        {
+          name: "Chiqui ",
+          lastname: "Rodriguez ",
+          type: "Dot",
+          age: 0,
+          rh: "0+",
+        },
+        {
+          name: "Pancho ",
+          lastname: "Rodriguez ",
+          type: "Hourse",
+          age: 0,
+          rh: "0+",
+        },
+        {
+          name: "Negro ",
+          lastname: "Rodriguez ",
+          type: "Cat",
+          age: 0,
+          rh: "0+",
+        },
+        {
+          name: "Pepe ",
+          lastname: "Rodriguez ",
+          type: "Dot",
+          age: 0,
+          rh: "0+",
+        },
+        {
+          name: "Akira ",
+          lastname: "Rodriguez ",
+          type: "Hourse",
+          age: 0,
+          rh: "0+",
+        },
+        {
+          name: "Gatis ",
+          lastname: "Rodriguez ",
+          type: "Hourse",
+          age: 0,
+          rh: "0+",
+        },
+      ],
+      filterField: ''
+    };
+  },
+   computed: {
+    filteredList() {
+      return this.animals.filter(animal => {
+        return animal.name.toLowerCase().includes(this.filterField.toLowerCase())
+      })
+    }
   }
-}
+
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
